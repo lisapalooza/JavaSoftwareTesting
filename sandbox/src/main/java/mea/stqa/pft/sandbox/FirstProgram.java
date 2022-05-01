c30bfd16afb524aaa920de625031e03480e528df
Elizaveta Maliugina <lisapalooza@yandex.ru>
2022-05-01 15:11:58 +0500

    Изменены настройки форматирования

--- sandbox/src/main/java/mea/stqa/pft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/mea/stqa/pft/sandbox/FirstProgram.java
@@ -1,7 +1,7 @@
 package mea.stqa.pft.sandbox;
 
 public class FirstProgram {
-public static void main(String[] args) {
-System.out.println("Hello,world!");
-}
+  public static void main(String[] args) {
+    System.out.println("Hello,world!");
+  }
 }
