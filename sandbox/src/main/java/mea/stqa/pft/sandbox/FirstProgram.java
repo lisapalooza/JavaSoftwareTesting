55664ac4bd83f6c7ba2e817f6da52f7e2ecd6d28
Elizaveta Maliugina <lisapalooza@yandex.ru>
2022-05-02 15:21:20 +0500

    Добавлено несколько функций

--- sandbox/src/main/java/mea/stqa/pft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/mea/stqa/pft/sandbox/FirstProgram.java
@@ -2,6 +2,30 @@ package mea.stqa.pft.sandbox;
 
 public class FirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello,world!");
+    hello("world");
+    hello("user");
+    hello("Alexei");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+
+    return a * b;
   }
 }
